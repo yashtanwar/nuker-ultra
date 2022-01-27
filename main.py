5bd046a5a02c3e4f56ab676865d3b498dace9168
Yash Tanwar <82315827+yashtanwar@users.noreply.github.com>
2022-01-27 13:47:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import json
 
-os.system('python nuker.py').
+os.system('python nuker.py')
 
 nuker1 = input("Enter your tokken: ")
 nuker2 = input("Enter server id: ")
 
 print("Started nuking ....")
-os.system('python nuker.py').
+os.system('python nuker.py')
 print("Done Nuking!")
